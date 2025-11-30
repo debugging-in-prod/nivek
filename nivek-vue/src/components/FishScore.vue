232c0cf527501caae7544d23880db27ac549490d
suuuth <kevin.sutherl@gmail.com>
2025-11-29 22:33:57 -0500

    frnt - grouping fish by name

--- nivek-vue/src/components/FishScore.vue
+++ nivek-vue/src/components/FishScore.vue
@@ -41,25 +41,48 @@ async function getFishScore() {
   }
 }
 
+// Group fish by name
+function groupFishByName(fish: Fish[]) {
+  const grouped: Record<string, { count: number; value: number; scarcity: number }> = {}
+
+  fish.forEach(f => {
+    if (!grouped[f.name]) {
+      grouped[f.name] = {
+        count: 0,
+        value: f.value,
+        scarcity: f.scarcity
+      }
+    }
+    grouped[f.name].count++
+  })
+
+  return grouped
+}
+
 onMounted(() => {
   getFishScore()
 })
 </script>
 
 <template>
-  <div v-for="fishScore in fishScores" class="text-center mb-5">
+  <div v-for="fishScore in fishScores" :key="fishScore.chatterName" class="text-center mb-5">
     <h3>Your Fish Score for <span>{{ fishScore.chatterName }}</span></h3>
     <p>Total Score: <span>{{ fishScore.score }}</span></p>
     <h4>Fish Caught</h4>
     <ul class="list-group">
-      <li v-for="fish in fishScore.fish" class="list-group-item">
-        <p>🐟<span>{{ fish.name }}</span> - <span>{{ fish.value }}</span></p>
-        <p>{{ fish.scarcity }}</p>
+      <li v-for="(groupedFish, name) in groupFishByName(fishScore.fish)"
+          :key="name"
+          class="list-group-item">
+        <p>🐟 <span>{{ name }}</span> × {{ groupedFish.count }} - <span>{{ groupedFish.value }}</span> points each</p>
+        <p>Rarity: {{ groupedFish.scarcity }}</p>
       </li>
     </ul>
   </div>
 </template>
 
 <style scoped>
-
+.list-group-item {
+  background: unset;
+  color: unset;
+}
 </style>
