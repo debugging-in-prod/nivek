facf56a078f3fe3abdbdfe8f2539533cda0f93bd
suuuth <kevin.sutherl@gmail.com>
2025-12-01 10:50:01 -0500

    auto shout css adjustments

--- nivek-vue/src/components/AutoShout.vue
+++ nivek-vue/src/components/AutoShout.vue
@@ -86,8 +86,8 @@ onMounted(() => {
     </div>
     <button type="submit" class="btn btn-primary mt-2">Add Chatter</button>
   </form>
-  <ul class="list-group">
-    <li v-for="chatter in autoShoutChatters" :key="chatter.id" class="list-group-item d-flex justify-content-between align-items-start">
+  <ul class="list-group list-group-flush">
+    <li v-for="chatter in autoShoutChatters" :key="chatter.id" class="list-group-item list-group-item-action d-flex justify-content-between align-items-start">
       <div>{{ chatter.chattername }}</div>
       <div class="text-end">
         <div>{{ chatter.shout_count }}</div>
