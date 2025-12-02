3b3bbc1009a3b2a9e2a284e06003179289f92185
suuuth <kevin.sutherl@gmail.com>
2025-12-02 09:09:17 -0500

    testing ci/cd agaain

--- cmd/core-api/endpoints/autoshout/update_chatter.go
+++ cmd/core-api/endpoints/autoshout/update_chatter.go
@@ -2,7 +2,6 @@ package autoshout
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -28,8 +27,6 @@ func NewUpdateAutoShoutChatterEndpoint(nivek nivek.NivekService) echo.HandlerFun
 		}
 
 		autoShoutService := autoShoutSvc.NewService(nivek)
-
-		log.Printf("hello world -- testing ci/cd automated building")
 
 		// get chatter record to make sure they exist first @TODO::update this to use a smaller fetch
 		chatter, errFetch := autoShoutService.GetAutoShoutChatter(user.Username, updatedChatter.ChatterName)
