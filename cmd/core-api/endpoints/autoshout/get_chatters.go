3789720a98d0e0de92637c00d9f11d7bae8646af
suuuth <kevin.sutherl@gmail.com>
2025-12-02 20:39:01 -0500

    debugging missing loglines

--- cmd/core-api/endpoints/autoshout/get_chatters.go
+++ cmd/core-api/endpoints/autoshout/get_chatters.go
@@ -5,34 +5,36 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	autoShoutSvc "github.com/tim-the-toolman-taylor/nivek/internal/libraries/autoshout"
 	"github.com/tim-the-toolman-taylor/nivek/internal/libraries/nivek"
-	"github.com/tim-the-toolman-taylor/nivek/internal/libraries/utilities"
 )
 
 func NewGetAutoShoutChattersEndpoint(nivek nivek.NivekService) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		user, err := utilities.GetUserFromContext(c)
-		if err != nil {
-			return c.JSON(http.StatusUnauthorized, map[string]string{
-				"error": "internal server error",
-			})
-		}
+		//user, err := utilities.GetUserFromContext(c)
+		//if err != nil {
+		//	return c.JSON(http.StatusUnauthorized, map[string]string{
+		//		"error": "internal server error",
+		//	})
+		//}
+
+		// these debug lines aren't printing
 
 		fmt.Println("hello world -- testing ci/cd automated building")
 		fmt.Println("hello worldX5 -- testing ci/cd automated building")
 
-		autoShoutService := autoShoutSvc.NewService(nivek)
-		chatters, errChat := autoShoutService.GetAutoShoutChatters(user.Username)
-		if errChat != nil {
-			return c.JSON(http.StatusInternalServerError, map[string]string{
-				"error": fmt.Sprintf(
-					"error fetching auto shout chatter for user [%s]: %s",
-					user.Username, errChat.Error(),
-				),
-			})
-		}
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "internal server error"})
 
-		return c.JSON(http.StatusOK, chatters)
+		//autoShoutService := autoShoutSvc.NewService(nivek)
+		//chatters, errChat := autoShoutService.GetAutoShoutChatters(user.Username)
+		//if errChat != nil {
+		//	return c.JSON(http.StatusInternalServerError, map[string]string{
+		//		"error": fmt.Sprintf(
+		//			"error fetching auto shout chatter for user [%s]: %s",
+		//			user.Username, errChat.Error(),
+		//		),
+		//	})
+		//}
+		//
+		//return c.JSON(http.StatusOK, chatters)
 	}
 }
