57c34bcad0adff736e98ff0520ff7e94fe778878
kevin <ne0ncoin@proton.me>
2025-12-23 20:44:27 -0500

    adjusting build and triggering another pipe

--- cmd/core-api/main.go
+++ cmd/core-api/main.go
@@ -34,13 +34,11 @@ func main() {
 				panic("failed to assert custom config")
 			}
 
-			fmt.Println("//")
 			fmt.Println("====================================================")
 			fmt.Println("====================================================")
 			fmt.Println("Hello World! Signed - ", nivek.CommonConfig().AppName)
 			fmt.Println("====================================================")
 			fmt.Println("====================================================")
-			fmt.Println("//")
 
 			//
 			// Start the API server
